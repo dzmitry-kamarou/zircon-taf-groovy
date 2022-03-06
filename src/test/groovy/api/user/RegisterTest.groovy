b8d88830ed3abf8ede7baa57d3cfb88eeb33aae9
dzmitry-kamarou <kosarev.gomel@gmail.com>
2022-03-06 15:07:30 +0300

    dev: add C19

--- src/test/groovy/api/user/RegisterTest.groovy
+++ src/test/groovy/api/user/RegisterTest.groovy
@@ -2,7 +2,9 @@ package api.user
 
 import app.api.flow.UserFlow
 import business.account.Account
+import business.account.AccountFactory
 import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
@@ -20,6 +22,18 @@ class RegisterTest {
         def message = new UserFlow().registerAccount account
         def reason = 'Account with blank email not registered'
         assertThat reason, message, is('Incorrect email or password')
+    }
+
+    @Test
+    @Tag('C19')
+    @Tag('api')
+    @Tag('regression')
+    void theSameAccountCantBeRegisteredTwice() {
+        def account = AccountFactory.registeredUser()
+        def userFlow = new UserFlow()
+        def message = userFlow.registerAccount account
+        def reason = 'Account with the same email can\'t be registered'
+        assertThat reason, message, is('There is already user with the same email')
     }
 
     private static List<Account> wrongAccountDataProvider() {
