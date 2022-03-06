0b94a52039dcbc37807ef29da3c7ae95382a9c1a
dzmitry-kamarou <kosarev.gomel@gmail.com>
2022-03-06 14:04:35 +0300

    dev: add C14

--- src/test/groovy/api/user/FindTest.groovy
+++ src/test/groovy/api/user/FindTest.groovy
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test
 
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.nullValue
 
 class FindTest {
 
@@ -20,5 +21,17 @@ class FindTest {
         def found = new UserFlow().findAccount account
         def reason = "Registered '${account.email}' account was found"
         assertThat reason, found.email, is(account.email)
+    }
+
+    @Test
+    @Tag('C14')
+    @Tag('api')
+    @Tag('regression')
+    @Tag('smoke')
+    void notRegisteredUserCantBeFound() {
+        def account = AccountFactory.randomAccount()
+        def found = new UserFlow().findAccount account
+        def reason = "Not registered '${account.email}' account was not found"
+        assertThat reason, found, is(nullValue())
     }
 }
